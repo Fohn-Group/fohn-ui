5f26619ee24e42fab9d02a21a196f7f5cf8b7fa1
Alain Belair <belair.alain@gmail.com>
2023-04-04 13:38:22 -0400

    revert comment CallbackTest

--- tests/Callback/CallbackTest.php
+++ tests/Callback/CallbackTest.php
@@ -26,154 +26,154 @@ class CallbackTest extends \PHPUnit\Framework\TestCase
         $ui->setApp(new MockApp(['registerShutdown' => false]));
     }
 
-//    public function testDataCallback(): void
-//    {
-//        $hasExecute = false;
-//        $trigger = 'data_tg';
-//        $cb = new Data(['urlTrigger' => $trigger]);
-//        // should not trigger
-//        $cb->onDataRequest(function () use (&$hasExecute) {
-//            $hasExecute = true;
-//
-//            return [];
-//        });
-//        $this->assertFalse($hasExecute);
-//
-//        // should trigger
-//        $_GET[Generic::URL_QUERY_TARGET] = $trigger;
-//        $_GET[$trigger] = Generic::DATA_TYPE;
-//
-//        $cb->onDataRequest(function () use (&$hasExecute) {
-//            $hasExecute = true;
-//
-//            return [];
-//        });
-//        $this->assertTrue($hasExecute);
-//    }
-//
-//    public function testGenericCallback(): void
-//    {
-//        $hasExecute = false;
-//        $trigger = 'generic_tg';
-//        $cb = new Generic(['urlTrigger' => $trigger]);
-//        // should not trigger
-//        $cb->onRequest(function () use (&$hasExecute) {
-//            $hasExecute = true;
-//
-//            return null;
-//        });
-//        $this->assertFalse($hasExecute);
-//
-//        // should trigger
-//        $_GET[Generic::URL_QUERY_TARGET] = $trigger;
-//        $_GET[$trigger] = Generic::GENERIC_TYPE;
-//
-//        $cb->onRequest(function () use (&$hasExecute) {
-//            $hasExecute = true;
-//
-//            return null;
-//        });
-//        $this->assertTrue($hasExecute);
-//    }
-//
-//    public function testAjaxCallback(): void
-//    {
-//        $hasExecute = false;
-//        $trigger = 'ajax_tg';
-//        $cb = new Ajax(['urlTrigger' => $trigger]);
-//        // should not trigger
-//        $cb->onAjaxPostRequest(function () use (&$hasExecute) {
-//            $hasExecute = true;
-//
-//            return null;
-//        });
-//        $this->assertFalse($hasExecute);
-//
-//        // should trigger
-//        $_GET[Generic::URL_QUERY_TARGET] = $trigger;
-//        $_GET[$trigger] = Generic::AJAX_TYPE;
-//
-//        $cb->onAjaxPostRequest(function () use (&$hasExecute) {
-//            $hasExecute = true;
-//
-//            return Js::from('');
-//        });
-//        $this->assertTrue($hasExecute);
-//    }
-//
-//    public function testJqueryCallback(): void
-//    {
-//        $hasExecute = false;
-//        $trigger = 'jquery_tg';
-//        $cb = new Jquery(['urlTrigger' => $trigger]);
-//        // should not trigger
-//        $cb->onJqueryRequest(function () use (&$hasExecute) {
-//            $hasExecute = true;
-//
-//            return null;
-//        });
-//        $this->assertFalse($hasExecute);
-//
-//        // should trigger
-//        $_GET[Generic::URL_QUERY_TARGET] = $trigger;
-//        $_GET[$trigger] = Generic::JQUERY_TYPE;
-//
-//        $cb->onJqueryRequest(function () use (&$hasExecute) {
-//            $hasExecute = true;
-//
-//            return Js::from('');
-//        });
-//        $this->assertTrue($hasExecute);
-//    }
-//
-//    public function testJqReloadCallback(): void
-//    {
-//        $hasExecute = false;
-//        $trigger = 'jr_reload_tg';
-//        $view = new View(['template' => new HtmlTemplate('<div></div>')]);
-//        $cb = JqReload::addAbstractTo($view, ['urlTrigger' => $trigger]);
-//
-//        // should not trigger
-//        $cb->onJqueryRequest(function () use (&$hasExecute) {
-//            $hasExecute = true;
-//
-//            return null;
-//        });
-//        $this->assertFalse($hasExecute);
-//
-//        // should trigger
-//        $_GET[Generic::URL_QUERY_TARGET] = $trigger;
-//        $_GET[$trigger] = Generic::JQUERY_TYPE;
-//
-//        $cb->onJqueryRequest(function () use (&$hasExecute) {
-//            $hasExecute = true;
-//        });
-//        $this->assertTrue($hasExecute);
-//    }
-//
-//    public function testServerEventCallback(): void
-//    {
-//        $hasExecute = false;
-//        $trigger = 'sse_tg';
-//        $cb = new ServerEvent(['urlTrigger' => $trigger, 'app' => new MockApp()]);
-//        $cb->invokeInitRenderTree();
-//
-//        // should not trigger
-//        $cb->onRequest(function () use (&$hasExecute) {
-//            $hasExecute = true;
-//
-//            return null;
-//        });
-//        $this->assertFalse($hasExecute);
-//
-//        // should trigger
-//        $_GET[Generic::URL_QUERY_TARGET] = $trigger;
-//        $_GET[$trigger] = Generic::SERVER_EVENT_TYPE;
-//
-//        $cb->onRequest(function (ServerEvent $event) use (&$hasExecute) {
-//            $hasExecute = true;
-//            $event->executeJavascript(Js::from('console.log()'));
-//        });
-//        $this->assertTrue($hasExecute);
-//    }
+    public function testDataCallback(): void
+    {
+        $hasExecute = false;
+        $trigger = 'data_tg';
+        $cb = new Data(['urlTrigger' => $trigger]);
+        // should not trigger
+        $cb->onDataRequest(function () use (&$hasExecute) {
+            $hasExecute = true;
+
+            return [];
+        });
+        $this->assertFalse($hasExecute);
+
+        // should trigger
+        $_GET[Generic::URL_QUERY_TARGET] = $trigger;
+        $_GET[$trigger] = Generic::DATA_TYPE;
+
+        $cb->onDataRequest(function () use (&$hasExecute) {
+            $hasExecute = true;
+
+            return [];
+        });
+        $this->assertTrue($hasExecute);
+    }
+
+    public function testGenericCallback(): void
+    {
+        $hasExecute = false;
+        $trigger = 'generic_tg';
+        $cb = new Generic(['urlTrigger' => $trigger]);
+        // should not trigger
+        $cb->onRequest(function () use (&$hasExecute) {
+            $hasExecute = true;
+
+            return null;
+        });
+        $this->assertFalse($hasExecute);
+
+        // should trigger
+        $_GET[Generic::URL_QUERY_TARGET] = $trigger;
+        $_GET[$trigger] = Generic::GENERIC_TYPE;
+
+        $cb->onRequest(function () use (&$hasExecute) {
+            $hasExecute = true;
+
+            return null;
+        });
+        $this->assertTrue($hasExecute);
+    }
+
+    public function testAjaxCallback(): void
+    {
+        $hasExecute = false;
+        $trigger = 'ajax_tg';
+        $cb = new Ajax(['urlTrigger' => $trigger]);
+        // should not trigger
+        $cb->onAjaxPostRequest(function () use (&$hasExecute) {
+            $hasExecute = true;
+
+            return null;
+        });
+        $this->assertFalse($hasExecute);
+
+        // should trigger
+        $_GET[Generic::URL_QUERY_TARGET] = $trigger;
+        $_GET[$trigger] = Generic::AJAX_TYPE;
+
+        $cb->onAjaxPostRequest(function () use (&$hasExecute) {
+            $hasExecute = true;
+
+            return Js::from('');
+        });
+        $this->assertTrue($hasExecute);
+    }
+
+    public function testJqueryCallback(): void
+    {
+        $hasExecute = false;
+        $trigger = 'jquery_tg';
+        $cb = new Jquery(['urlTrigger' => $trigger]);
+        // should not trigger
+        $cb->onJqueryRequest(function () use (&$hasExecute) {
+            $hasExecute = true;
+
+            return null;
+        });
+        $this->assertFalse($hasExecute);
+
+        // should trigger
+        $_GET[Generic::URL_QUERY_TARGET] = $trigger;
+        $_GET[$trigger] = Generic::JQUERY_TYPE;
+
+        $cb->onJqueryRequest(function () use (&$hasExecute) {
+            $hasExecute = true;
+
+            return Js::from('');
+        });
+        $this->assertTrue($hasExecute);
+    }
+
+    public function testJqReloadCallback(): void
+    {
+        $hasExecute = false;
+        $trigger = 'jr_reload_tg';
+        $view = new View(['template' => new HtmlTemplate('<div></div>')]);
+        $cb = JqReload::addAbstractTo($view, ['urlTrigger' => $trigger]);
+
+        // should not trigger
+        $cb->onJqueryRequest(function () use (&$hasExecute) {
+            $hasExecute = true;
+
+            return null;
+        });
+        $this->assertFalse($hasExecute);
+
+        // should trigger
+        $_GET[Generic::URL_QUERY_TARGET] = $trigger;
+        $_GET[$trigger] = Generic::JQUERY_TYPE;
+
+        $cb->onJqueryRequest(function () use (&$hasExecute) {
+            $hasExecute = true;
+        });
+        $this->assertTrue($hasExecute);
+    }
+
+    public function testServerEventCallback(): void
+    {
+        $hasExecute = false;
+        $trigger = 'sse_tg';
+        $cb = new ServerEvent(['urlTrigger' => $trigger, 'app' => new MockApp()]);
+        $cb->invokeInitRenderTree();
+
+        // should not trigger
+        $cb->onRequest(function () use (&$hasExecute) {
+            $hasExecute = true;
+
+            return null;
+        });
+        $this->assertFalse($hasExecute);
+
+        // should trigger
+        $_GET[Generic::URL_QUERY_TARGET] = $trigger;
+        $_GET[$trigger] = Generic::SERVER_EVENT_TYPE;
+
+        $cb->onRequest(function (ServerEvent $event) use (&$hasExecute) {
+            $hasExecute = true;
+            $event->executeJavascript(Js::from('console.log()'));
+        });
+        $this->assertTrue($hasExecute);
+    }
 }
