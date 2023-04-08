da5eab52190841bba7ec7b2932a011d8571b08a4
Alain Belair <belair.alain@gmail.com>
2023-04-08 12:16:15 -0400

    Create AppTest.php

---
+++ tests/DemoApp/AppTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types = 1);
+/**
+ * Test app-test folder.
+ */
+
+namespace Fohn\Ui\Tests\DemoApp;
+
+use GuzzleHttp\Client;
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase
+{
+    protected const ROOT_DIR = __DIR__ . '/../..';
+    protected const APP_DIR = self::ROOT_DIR . '/app-test';
+
+    protected Client $client;
+
+    protected function setUp(): void
+    {
+        require_once __DIR__ . '/init-configuration.php';
+
+        $this->client = new Client(
+            [
+                'base_uri' => loadConfig()['base_uri'],
+                'headers' => ['x-coverage-id' => 'pcov'],
+            ]
+        );
+    }
+
+    public function demoFilesProvider(): array
+    {
+        $excludeDirs = ['_app-data', '_includes', 'local'];
+        $excludeFiles = [];
+
+        $files = [];
+        $files[] = 'index.php';
+        foreach (array_diff(scandir(static::APP_DIR), ['.', '..'], $excludeDirs) as $dir) {
+            if (!is_dir(static::APP_DIR . '/' . $dir)) {
+                continue;
+            }
+
+            foreach (scandir(static::APP_DIR . '/' . $dir) as $f) {
+                $path = $dir . '/' . $f;
+                if (substr($path, -4) !== '.php' || in_array($path, $excludeFiles, true)) {
+                    continue;
+                }
+
+                $files[] = $path;
+            }
+        }
+
+        return array_reduce($files, function (array $items, string $v) {
+            $items[$v] = [$v];
+
+            return $items;
+        }, []);
+    }
+
+    /**
+     * @dataProvider demoFilesProvider
+     */
+    public function testAppTestHtmlResponse(string $uri): void
+    {
+        $request = new \GuzzleHttp\Psr7\Request('GET', '/app-test/' . $uri);
+        $response = $this->client->send($request);
+
+        $this->assertSame(200, $response->getStatusCode(), ' Status error on ' . $uri);
+    }
+}
