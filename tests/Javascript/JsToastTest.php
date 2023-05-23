8a760fcf776527c8c6e994504e712e41285abe91
Alain Belair <belair.alain@gmail.com>
2023-05-23 09:43:10 -0400

    fix jsToast test

--- tests/Javascript/JsToastTest.php
+++ tests/Javascript/JsToastTest.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 /**
- * Created by abelair.
- * Date: 2023-02-27
- * Time: 10:13 a.m.
+ * JsToast
  */
 
 namespace Fohn\Ui\Tests\Javascript;
@@ -16,16 +14,28 @@ class JsToastTest extends TestCase
 {
     public function testJsToast(): void
     {
-        $expected = "fohn.toastService.notify('title','a message',{type:'success'})";
+        $expected = "fohn.toastService.notify('title','a message',{},false)";
+        $this->assertSame($expected, JsToast::notify('title', 'a message', [], false)->jsRender());
+
+        $expected = "fohn.toastService.notify('title','a message',{a:'v'},true)";
+        $this->assertSame($expected, JsToast::notify('title', 'a message', ['a' => 'v'])->jsRender());
+
+        $expected = "fohn.toastService.notify('title','a message',{type:'success'},true)";
         $this->assertSame($expected, JsToast::success('title', 'a message')->jsRender());
 
-        $expected = "fohn.toastService.notify('title','a message',{type:'error'})";
+        $expected = "fohn.toastService.notify('title','a message',{a:'v',type:'success'},true)";
+        $this->assertSame($expected, JsToast::success('title', 'a message', ['a' => 'v'])->jsRender());
+
+        $expected = "fohn.toastService.notify('title','a message',{type:'success'},false)";
+        $this->assertSame($expected, JsToast::success('title', 'a message', [], false)->jsRender());
+
+        $expected = "fohn.toastService.notify('title','a message',{type:'error'},true)";
         $this->assertSame($expected, JsToast::error('title', 'a message')->jsRender());
 
-        $expected = "fohn.toastService.notify('title','a message',{type:'warning'})";
+        $expected = "fohn.toastService.notify('title','a message',{type:'warning'},true)";
         $this->assertSame($expected, JsToast::warning('title', 'a message')->jsRender());
 
-        $expected = "fohn.toastService.notify('title','a message',{type:'info'})";
+        $expected = "fohn.toastService.notify('title','a message',{type:'info'},true)";
         $this->assertSame($expected, JsToast::info('title', 'a message')->jsRender());
     }
 }
