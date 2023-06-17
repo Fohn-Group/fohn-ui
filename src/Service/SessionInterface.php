e6d303438d6ba1f2cab51587215db0317dcd72a8
Alain Belair <belair.alain@gmail.com>
2023-06-17 13:53:30 -0400

    Add session and session interface

---
+++ src/Service/SessionInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * Session Interface.
+ */
+
+namespace Fohn\Ui\Service;
+
+interface SessionInterface
+{
+    /**
+     * Set session option.
+     */
+    public function setOptions(array $options): void;
+
+    public function set(string $key, string $value, bool $keepOpen = false): void;
+
+    public function setMultiple(array $values, bool $keepOpen = false): void;
+
+    public function retrieve(string $key, string $default = null): ?string;
+
+    public function forget(string $key = null): void;
+
+    public function get(string $key, string $default = null): ?string;
+
+    public function body(bool $clear = false): array;
+
+    public function destroy(): void;
+}
