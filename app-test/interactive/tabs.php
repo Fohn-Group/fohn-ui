afce1e603328b36c3f66d39053bae1773a10cb89
Alain Belair <belair.alain@gmail.com>
2023-07-28 09:26:29 -0400

    add demo

--- app-test/interactive/tabs.php
+++ app-test/interactive/tabs.php
@@ -2,22 +2,47 @@
 
 declare(strict_types = 1);
 
+use Fohn\Ui\AppTest\Model\Country;
+use Fohn\Ui\Component\Form;
 use Fohn\Ui\Component\Tab\Tab;
 use Fohn\Ui\Component\Tabs;
+use Fohn\Ui\Js\Jquery;
+use Fohn\Ui\Js\JsReload;
+use Fohn\Ui\Js\JsToast;
+use Fohn\Ui\Service\Atk\FormModelController;
+use Fohn\Ui\Service\Data;
 use Fohn\Ui\Service\Ui;
 use Fohn\Ui\View;
+use Fohn\Ui\View\Button;
 
 require_once __DIR__ . '/../init-ui.php';
 
+$modelCtrl = new FormModelController(new Country(Data::db()));
+$id = (string) $modelCtrl->getModel()->tryLoadAny()->get('id');
+
 $tabs = Tabs::addTo(Ui::layout());
 
-$tab = $tabs->addTab(new Tab(['name' => 'home']));
+$homeTab = $tabs->addTab(new Tab(['name' => 'home']));
 
-$tab->addView((new View())->setTextContent('This is home tab'));
+View::addTo($homeTab)->setTextContent('This is home tab content.');
 
-$tab = $tabs->addTab(new Tab(['name' => 'profile']));
+$profileTab = $tabs->addTab(new Tab(['name' => 'profile']));
 
+$form = Form::addTo($profileTab);
+$form->addControls($modelCtrl->factoryFormControls($id));
+$form->onSubmit(function (Form $f) use ($modelCtrl, $id) {
+    if ($errors = $modelCtrl->saveModelUsingForm($id, $f->getControls())) {
+        $f->addValidationErrors($errors);
+    }
 
-$tab->addView((new View())->setTextContent('This is profile tab'));
+    return JsToast::success('Saved!');
+});
+
+$userTab = $tabs->addTab(new Tab(['name' => 'user']));
+View::addTo($userTab)->setTextContent('This is user tab content.');
+$b = Button::addTo($userTab, ['label' => 'Reload ' . ($_GET['test'] ?? 0)]);
+Jquery::addEventTo($b, 'click')->execute(JsReload::view($b, ['test ' => random_int(0, 100)]));
+
+$tabs->activateTabName('profile');
 
 Ui::viewDump($tabs, 'tab');
