7ef350344d11e6908760df33d0cd1bde06af2130
Alain Belair <belair.alain@gmail.com>
2023-08-01 15:52:08 -0400

    update Page for proper package version

--- src/Page.php
+++ src/Page.php
@@ -37,7 +37,7 @@ class Page extends View
             'url' => 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js',
         ],
         'fohn-js' => [
-            'url' => 'https://unpkg.com/fohn-ui@1.3.1/dist/fohn-ui.min.js',
+            'url' => 'https://unpkg.com/fohn-ui@1.4.0/dist/fohn-ui.min.js',
         ],
     ];
 
@@ -50,7 +50,7 @@ class Page extends View
             'url' => 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css',
         ],
         'fohn-css' => [
-            'url' => 'https://unpkg.com/fohn-ui-css@1.2.0/dist/fohn-ui.min.css',
+            'url' => 'https://unpkg.com/fohn-ui-css@1.3.0/dist/fohn-ui.min.css',
         ],
     ];
 
