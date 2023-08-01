f7875caf049b5718df68fdd3aac7e9839e7b115c
Alain Belair <belair.alain@gmail.com>
2023-08-01 15:32:33 -0400

    add event handler js function

--- src/Component/Tab/Tab.php
+++ src/Component/Tab/Tab.php
@@ -10,6 +10,7 @@ namespace Fohn\Ui\Component\Tab;
 use Fohn\Ui\Component\VueInterface;
 use Fohn\Ui\Component\VueTrait;
 use Fohn\Ui\Js\Js;
+use Fohn\Ui\Js\JsFunction;
 use Fohn\Ui\View;
 
 class Tab extends View implements VueInterface
@@ -17,6 +18,10 @@ class Tab extends View implements VueInterface
     use VueTrait;
 
     protected const COMP_NAME = 'fohn-tab';
+    private const FN_INIT_KEY = 'init';
+    private const FN_SHOW_KEY = 'show';
+    private const FN_HIDE_KEY = 'hide';
+
 
     public string $defaultTemplate = 'vue-component/tabs/tab.html';
 
@@ -24,6 +29,10 @@ class Tab extends View implements VueInterface
 
     protected string $name = '';
     protected string $caption = '';
+    protected bool $isDisabled = false;
+
+    /** @var array<JsFunction> JsFunction to execute when this tab become active. */
+    private array $onActiveHandlers = [];
 
     protected function initRenderTree(): void
     {
@@ -40,6 +49,25 @@ class Tab extends View implements VueInterface
         return $this->caption;
     }
 
+    public function disabled(): self
+    {
+        $this->isDisabled = true;
+
+        return $this;
+    }
+
+    public function enable(): self
+    {
+        $this->isDisabled = false;
+
+        return $this;
+    }
+
+    public function isDisabled(): bool
+    {
+        return $this->isDisabled;
+    }
+
     public function setName(string $name): self
     {
         $this->name = $name;
@@ -54,10 +82,43 @@ class Tab extends View implements VueInterface
         return $this;
     }
 
+    public function jsOnInitTab(JsFunction $fn): JsFunction
+    {
+        $this->appendJsActiveHandlerFn($fn, self::FN_INIT_KEY);
+
+        return $fn;
+    }
+
+    public function jsOnShowTab(JsFunction $fn): JsFunction
+    {
+        $this->appendJsActiveHandlerFn($fn, self::FN_SHOW_KEY);
+
+        return $fn;
+    }
+
+    public function jsOnHideTab(JsFunction $fn): JsFunction
+    {
+        $this->appendJsActiveHandlerFn($fn, self::FN_HIDE_KEY);
+
+        return $fn;
+    }
+
+    /**
+     * Add a Js Function to be executed when this tab become active.
+     * When $always is set to true, the Js function will run each time the tab become active,
+     * otherwise, it will run only once.
+     */
+    private function appendJsActiveHandlerFn(JsFunction $fn, string $key): void
+    {
+        $this->onActiveHandlers[] = [$key => $fn];
+    }
+
+
     protected function beforeHtmlRender(): void
     {
         $this->getTemplate()->trySetJs('tabName', Js::string($this->getName()));
         $this->getTemplate()->trySetJs('tabStoreId', Js::string($this->tabStoreId));
+        $this->getTemplate()->trySetJs('onActiveHandlers', Js::array($this->onActiveHandlers));
 
         parent::beforeHtmlRender();
     }
