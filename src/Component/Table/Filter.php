b85c5c04cf2c065526678c2eda979af98c0931cf
Alain Belair <belair.alain@gmail.com>
2024-06-12 19:08:35 -0400

    Update Filter.php

--- src/Component/Table/Filter.php
+++ src/Component/Table/Filter.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace Fohn\Ui\Component\Table;
 
+use Fohn\Ui\Component\Table\Filter\FilterInterface;
 use Fohn\Ui\Component\VueTrait;
 use Fohn\Ui\Js\Js;
 use Fohn\Ui\View;
@@ -21,6 +22,20 @@ class Filter extends View
     public const FILTER_PROP_ICON_NAME = 'icon-name';
     public const FILTER_PROP_ALT_ICON_NAME = 'alt-icon-name';
 
+    protected const INPUT_VUE_COMPONENT = 'input';
+    protected const DATE_VUE_COMPONENT = 'flat-pickr';
+
+    /**
+     * Vue component to use according to data type.
+     */
+    protected const VUE_COMPONENT_NAME_TYPE = [
+        'text' => self::INPUT_VUE_COMPONENT,
+        'number' => self::INPUT_VUE_COMPONENT,
+        'date' => self::DATE_VUE_COMPONENT,
+        'datetime' => self::DATE_VUE_COMPONENT,
+        'time' => self::DATE_VUE_COMPONENT,
+    ];
+
     public string $defaultTemplate = 'vue-component/table/table-filter.html';
     public array $defaultTailwind = [
         'mx-2',
@@ -32,40 +47,90 @@ class Filter extends View
     protected string $iconName = 'bi bi-funnel';
     protected string $altIconName = 'bi bi-funnel-fill';
 
-    protected function initRenderTree(): void
+    /** @var array<string, FilterInterface> */
+    protected array $filters = [];
+
+    protected array $matchTypes = [
+      ['id' => 'and', 'label' => 'And'],
+      ['id' => 'or', 'label' => 'Or'],
+    ];
+
+    protected array $operators = [
+        ['id' => 'contains', 'label' => 'Contains', 'types' => ['text'], 'requiredValue' => true],
+        ['id' => 'notContains', 'label' => 'Not Contains', 'types' => ['text'], 'requiredValue' => true],
+        ['id' => 'startsWith', 'label' => 'Starts With', 'types' => ['text'], 'requiredValue' => true],
+        ['id' => 'endsWith', 'label' => 'End With', 'types' => ['text'], 'requiredValue' => true],
+        ['id' => 'equals', 'label' => 'Equals', 'types' => ['text'], 'requiredValue' => true],
+        ['id' => '=', 'label' => '=', 'types' => ['number'], 'requiredValue' => true],
+        ['id' => '!=', 'label' => '!=', 'types' => ['number'], 'requiredValue' => true],
+        ['id' => '>', 'label' => '>', 'types' => ['number'], 'requiredValue' => true],
+        ['id' => '>=', 'label' => '>=', 'types' => ['number'], 'requiredValue' => true],
+        ['id' => '<', 'label' => '<', 'types' => ['number'], 'requiredValue' => true],
+        ['id' => '<=', 'label' => '<=', 'types' => ['number'], 'requiredValue' => true],
+        ['id' => 'is', 'label' => 'Is', 'types' => ['date', 'datetime', 'time', 'boolean'], 'requiredValue' => true],
+        ['id' => 'isNot', 'label' => 'Is Not', 'types' => ['date', 'datetime', 'time'], 'requiredValue' => true],
+        ['id' => 'isAfter', 'label' => 'Is After', 'types' => ['date', 'datetime', 'time'], 'requiredValue' => true],
+        ['id' => 'isOnOrAfter', 'label' => 'Is On Or After', 'types' => ['date', 'datetime', 'time'], 'requiredValue' => true],
+        ['id' => 'isBefore', 'label' => 'Is Before', 'types' => ['date', 'datetime', 'time'], 'requiredValue' => true],
+        ['id' => 'isOnOrBefore', 'label' => 'Is On Or Before', 'types' => ['date', 'datetime', 'time'], 'requiredValue' => true],
+        ['id' => 'isEmpty', 'label' => 'Is Empty', 'types' => ['text', 'number', 'date', 'datetime', 'time'], 'requiredValue' => false],
+        ['id' => 'isNotEmpty', 'label' => 'Is Not Empty', 'types' => ['text', 'number', 'date', 'datetime', 'time'], 'requiredValue' => false],
+        //        ['id' => 'isAnyOf', 'label' => 'Is Any Of', 'types' => ['text', 'number'], 'requiredValue' => true], To add with multiple value component
+    ];
+
+    public static function getComponentName(string $type): string
     {
-        parent::initRenderTree();
+        return static::VUE_COMPONENT_NAME_TYPE[$type];
+    }
 
-        $this->addProperty(self::FILTER_PROP_ICON_NAME, Js::string($this->iconName));
-        $this->addProperty(self::FILTER_PROP_ALT_ICON_NAME, Js::string($this->altIconName));
-        $this->addProperty('columns', Js::array([
-            ['id' => 'id', 'label' => 'Id', 'componentName' => 'input', 'operatorType' => 'number', 'props' => ['type' => 'number', 'name' => 'id']],
-            ['id' => 'name', 'label' => 'Name', 'componentName' => 'input', 'operatorType' => 'text', 'props' => ['type' => 'text', 'name' => 'name']],
-            ['id' => 'date', 'label' => 'Date', 'componentName' => 'flat-pickr', 'operatorType' => 'date', 'props' => ['config' => ['format' => 'Y-m-d']]],
-        ]));
-        $this->addProperty('operators', Js::array([
-            ['id' => '=', 'label' => '=', 'types' => ['number', 'date']],
-            ['id' => '>', 'label' => '>', 'types' => ['number', 'date']],
-            ['id' => 'contains', 'label' => 'contains', 'types' => ['text']],
-            ['id' => 'notContains', 'label' => 'not contains', 'types' => ['text']],
-            ['id' => 'startsWith', 'label' => 'starts with', 'types' => ['text']],
-            ['id' => 'endsWith', 'label' => 'end with', 'types' => ['text']],
-            ['id' => 'equals', 'label' => 'equals', 'types' => ['text']],
-            ['id' => 'isEmpty', 'label' => 'is empty', 'types' => ['text', 'number']],
-            ['id' => 'isNotEmpty', 'label' => 'is not empty', 'types' => ['text', 'number']],
-            ['id' => 'isAnyOf', 'label' => 'is any of', 'types' => ['text', 'number']],
-        ]));
+    public function addFilter(FilterInterface $filter): FilterInterface
+    {
+        $this->filters[$filter->getId()] = $filter;
 
-        $this->addEvent('click', Js::var(self::FILTER_EVENT_TOGGLE));
+        return $filter;
+    }
+
+    /**
+     * Provide an array of operator [id => label] for translation.
+     * Ex: setOperatorsLabel(['contains' => 'Contient'])
+     */
+    public function setOperatorsLabels(array $operators): self
+    {
+        foreach ($operators as $id => $label) {
+            // find operators Id in list and set label
+            for ($i = 0; $i < count($this->operators); $i++) {
+                if ($this->operators[$i]['id'] === $id) {
+                    $this->operators[$i]['label'] = $label;
+                }
+            }
+        }
+
+        return $this;
     }
 
     protected function beforeHtmlRender(): void
     {
+        $this->addProperty('columns', Js::array($this->getFitlersColumn()));
+        $this->addProperty('operators', Js::array($this->operators));
+        $this->addProperty('match-types', Js::array($this->matchTypes));
+        $this->addProperty(self::FILTER_PROP_ICON_NAME, Js::string($this->iconName));
+        $this->addProperty(self::FILTER_PROP_ALT_ICON_NAME, Js::string($this->altIconName));
+
         $this->renderEvents();
         $this->renderProperties();
 
         $this->createVueApp(self::COMP_NAME, [], $this->getDefaultSelector());
 
         parent::beforeHtmlRender();
+    }
+
+    private function getFitlersColumn(): array
+    {
+        $columns = [];
+        foreach ($this->filters as $filter) {
+            $columns[] = $filter->getDefinition();
+        }
+
+        return $columns;
     }
 }
