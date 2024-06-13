5b55caa152c865c9c43b146f53bdab58c20cc461
Alain Belair <belair.alain@gmail.com>
2024-06-13 09:17:49 -0400

    Update table-filter.php

--- app-test/_unit-test/table-filter.php
+++ app-test/_unit-test/table-filter.php
@@ -14,7 +14,6 @@ require_once __DIR__ . '/../init-ui.php';
 // \Fohn\Ui\Component\Utils::requireFLatPickrLocale(\Fohn\Ui\Service\Ui::page(), 'fr', );
 
 $filter = \Fohn\Ui\Component\Table\Filter::addTo(Ui::layout());
-$filter->setOperatorsLabels(['is' => 'Isss', 'contains' => 'Containsss']);
 
 $filter->addFilter(new Number('id'));
 $filter->addFilter(new Text('name'));
