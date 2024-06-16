d595878da1073914bab0f3b744bdbd9b5ca7680c
Alain Belair <belair.alain@gmail.com>
2024-06-16 15:39:42 -0400

    use value from table filter column instead of supply value for payload

--- src/Component/Table/Payload.php
+++ src/Component/Table/Payload.php
@@ -14,15 +14,25 @@ class Payload
     public string $sortDirection;
     public int $ipp;
     public array $filters;
+
     public string $searchQuery;
 
-    public function __construct(array $payload)
+    public function __construct(array $payload, FilterInterface $filter)
     {
         $this->page = $payload['page'] ?? 0;
         $this->sortColumn = $payload['sorting']['columnName'] ?? '';
         $this->sortDirection = $payload['sorting']['direction'] ?? '';
         $this->ipp = $payload['ipp'] ?? 10;
         $this->searchQuery = $payload['_q'] ?? '';
-        $this->filters = $payload['filters'] ?? [];
+        $this->filters = $this->getFilterValues($payload['filters'] ?? [], $filter);
+    }
+
+    private function getFilterValues(array $filterPayload, FilterInterface $filter): array
+    {
+        foreach ($filterPayload['columns'] as $k => $col) {
+            $filterPayload['columns'][$k]['filterValue'] = $filter->getColumnFilter($col['column'])->getValue($col['value']);
+        }
+
+        return $filterPayload;
     }
 }
