8a29cc1d123a14bf98448477f861b779ae7870c9
Alain Belair <belair.alain@gmail.com>
2024-08-07 14:53:45 -0400

    improve demo

--- app-test/collection/tree.php
+++ app-test/collection/tree.php
@@ -12,16 +12,65 @@ use Fohn\Ui\Service\Ui;
 
 require_once __DIR__ . '/../init-ui.php';
 
+$mode = $_GET['mode'] ?? 'single';
+$hideFolder = ($_GET['folder'] ?? null) === null;
+
+// some file selection in checkbox mode.
+$treeValue = [
+    159 => [
+        'checked' => true,
+        'partialChecked' => false,
+    ],
+    160 => [
+        'checked' => true,
+        'partialChecked' => false,
+    ],
+    161 => [
+        'checked' => true,
+        'partialChecked' => false,
+    ],
+    162 => [
+        'checked' => true,
+        'partialChecked' => false,
+    ],
+    163 => [
+        'checked' => true,
+        'partialChecked' => false,
+    ],
+    164 => [
+        'checked' => true,
+        'partialChecked' => false,
+    ],
+    165 => [
+        'checked' => true,
+        'partialChecked' => false,
+    ],
+    166 => [
+        'checked' => false,
+        'partialChecked' => true,
+    ],
+    167 => [
+        'checked' => true,
+        'partialChecked' => false,
+    ],
+    168 => [
+        'checked' => true,
+        'partialChecked' => false,
+    ],
+];
+
 $fileModel = new File(Data::db());
 $tree = Tree::addTo(Ui::layout());
-
-$tree->setNodes($fileModel->getFilesHierarchy());
-$tree->setSelectionMode($_GET['mode'] ?? 'single')
+if ($mode === 'checkbox') {
+    $tree->setValue($treeValue);
+}
+$tree->setNodes($fileModel->getFilesHierarchy($hideFolder));
+$tree->setSelectionMode($mode)
     ->setHeight('600px')
     ->setFilter(['label', 'type'], 'lenient', 'Search file');
 
-$tree->onNodeSelected(static function ($tree, $key) {
-    return JsToast::notify('node selected', $key);
+$tree->onTreeNodeChanged(static function ($action, $key, $selected, $newTreeValue) {
+    return JsToast::notify(ucfirst($action), 'Key: ' . $key);
 });
 
 Ui::viewDump($tree, 'tree');
