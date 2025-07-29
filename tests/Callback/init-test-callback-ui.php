303a5b0f1477c6e0a729eba582a83b457ec0a82f
Alain Belair <belair.alain@gmail.com>
2025-07-29 15:37:22 +0000

    readd coverage for payload test

--- tests/Callback/init-test-callback-ui.php
+++ tests/Callback/init-test-callback-ui.php
@@ -19,10 +19,10 @@ $config = loadConfig();
 Ui::service()->boot(static function (Ui $ui) use ($config) {
     $app = new App(['registerShutdown' => false]);
 
-    // HttpCoverage::start();
-    // $app->onHooks(App::HOOKS_BEFORE_EXIT, static function () {
-    //     HttpCoverage::saveData();
-    // });
+    HttpCoverage::start();
+    $app->onHooks(App::HOOKS_BEFORE_EXIT, static function () {
+        HttpCoverage::saveData();
+    });
 
     date_default_timezone_set($config['timezone']);
     Data::setDb($config['db']);
